2fb3148418186c54608970f857188100b1dc25cc
RohitBCA456 <ry2281104@gmail.com>
2026-03-08 18:23:44 +0530

    increase the proxy timeout to handle render cold start

--- services/api-gateway/app.js
+++ services/api-gateway/app.js
@@ -5,26 +5,53 @@ import cors from "cors";
 
 dotenv.config();
 const app = express();
+
+const WAKE_UP_TIMEOUT = 30000;
+
 app.use(
   cors({
     origin: process.env.ALLOWED_ORIGIN,
     credentials: true,
-    methods: ["GET, POST, PUT, DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
-app.use("/memory-service", proxy(process.env.MEMORY_ROUTE));
-app.use("/retrieval-service", proxy(process.env.RETRIEVAL_ROUTE));
-app.use("/deletion-service", proxy(process.env.DELETION_ROUTE));
-app.use("/user-service", proxy(process.env.USER_ROUTE, {
-  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
-    if (srcReq.headers['authorization']) {
-      proxyReqOpts.headers['authorization'] = srcReq.headers['authorization'];
+const commonProxyOptions = {
+  proxyTimeout: WAKE_UP_TIMEOUT,
+  timeout: WAKE_UP_TIMEOUT,
+  proxyErrorHandler: (err, res, next) => {
+    if (err.code === "ECONNABORTED" || err.code === "ETIMEDOUT") {
+      return res
+        .status(504)
+        .send("Service is waking up, please try again in a few seconds.");
     }
-    return proxyReqOpts;
-  }
-}));
+    next(err);
+  },
+};
+
+app.use("/memory-service", proxy(process.env.MEMORY_ROUTE, commonProxyOptions));
+app.use(
+  "/retrieval-service",
+  proxy(process.env.RETRIEVAL_ROUTE, commonProxyOptions),
+);
+app.use(
+  "/deletion-service",
+  proxy(process.env.DELETION_ROUTE, commonProxyOptions),
+);
+
+app.use(
+  "/user-service",
+  proxy(process.env.USER_ROUTE, {
+    ...commonProxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      if (srcReq.headers["authorization"]) {
+        proxyReqOpts.headers["authorization"] = srcReq.headers["authorization"];
+      }
+      return proxyReqOpts;
+    },
+  }),
+);
 
 app.listen(process.env.PORT, () => {
   console.log("Gateway service running on port : ", process.env.PORT);
