c4746251bea08d319fa4dc29eb7f8ecedade8c67
RohitBCA456 <ry2281104@gmail.com>
2026-03-25 12:51:07 +0530

    updated sdk package

--- ai-memory-sdk/test-sdk.js
+++ ai-memory-sdk/test-sdk.js
@@ -2,7 +2,7 @@ import AIMemoryClient from "./index.js";
 
 // Initialize with only the API Key
 const client = new AIMemoryClient(
-  "cb7202a9c8ce550edd5e164def593d6ef1db27189a979ba9faed97510f046983",
+  "YOUR_API_KEY",
 );
 
 async function runTest() {
@@ -10,7 +10,7 @@ async function runTest() {
   try {
     console.log("📥 Testing Ingestion...");
     const ingestResult = await client.ingest(
-      "69ad631cb4d96dc4cad9e2e1",
+      "user_id",
       "I work at hcl as a software developer",
     );
     console.log(
@@ -19,7 +19,7 @@ async function runTest() {
     );
 
     console.log("Testing Retrieval...");
-    const retrieveResult = await client.retrieve("69ad631cb4d96dc4cad9e2e1", "do you know where i work at?");
+    const retrieveResult = await client.retrieve("user_id", "do you know where i work at?");
     console.log("Retrieval Success, Data: ", retrieveResult);
   } catch (error) {
     console.error("Test Failed:", error.message);
